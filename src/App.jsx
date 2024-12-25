8949c348dc680ba38af3c43a706c618d23e6111c
asadbekumarov <asadbekumarov922@gmail.com>
2024-12-26 01:00:06 +0500

    edn

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Create from "./components/createUser/Create";
 import Users from "./components/users/Users";
-import Layout from "./components/layout/LAyout";
+import Layout from "./components/layout/Layout";
 import { useState } from "react";
 function App() {
   const [users, setUsers] = useState([]);
